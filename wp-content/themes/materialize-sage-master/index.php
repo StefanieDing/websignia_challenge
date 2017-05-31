42b25310870606d20f0029aea7b16af9b80f6c1a
Stefanie Ding <StefanieDing@StefaniDingsMBP.home>
2017-05-31 19:14:35 -0400

    styled header

--- wp-content/themes/materialize-sage-master/index.php
+++ wp-content/themes/materialize-sage-master/index.php
@@ -1,9 +1,7 @@
-<?php get_template_part('templates/head', 'header'); ?>
+<?php get_template_part('header'); ?>
+
+<?php get_template_part('footer'); ?>
 
 
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', 'single'); ?>
-<?php endwhile; ?>
 
-<?php the_posts_navigation(); ?>
--- wp-content/themes/materialize-sage-master/templates/header.php
+++ wp-content/themes/materialize-sage-master/templates/header.php
@@ -1,13 +1,21 @@
 <nav>
   <div class="nav-wrapper">
-    <a href="#!" class="brand-logo left"><i class="material-icons">brightness_1</i></a>
-    <ul class="right hide-on-med-and-down">
-      <li>MENU</li>
-      <li><a href=""><i class="material-icons">reorder</i></a></li>
-    </ul>
-    <div class="nav-content center-align">
-      <h1>MAIN TITLE</h1>
-      <h2>Lorem ipsum dolor sit amet, eu est summo scripserit. Mei cu quod vol uptua</h2>
+    <div class="row">
+      <div class="col s3 left-align">
+        <a href="#!"><i class="large material-icons">brightness_1</i></a>
+      </div>
+      <div class="col s6 center-align">
+         <div class="title">
+          <h1><b>MAIN TITLE</b></h1>
+         </div>
+         <h2>Lorem ipsum dolor sit amet, eu est summo scripserit. Mei cu quod vol uptua</h2>
+      </div>
+      <div class="col s3 right-align">
+        <ul class="hide-on-med-and-down">
+          <li><b>MENU</b></li>
+          <li><a href=""><i class="medium material-icons">reorder</i></a></li>
+        </ul>
+      </div>
     </div>
   </div>
 </nav>
