5c0995d3f5c9acaba75a48748e132827bd674f8f
Stefanie Ding <StefanieDing@StefaniDingsMBP.home>
2017-05-31 23:46:10 -0400

    styling header

--- wp-content/themes/materialize-sage-master/templates/header.php
+++ wp-content/themes/materialize-sage-master/templates/header.php
@@ -2,7 +2,8 @@
   <div class="nav-wrapper">
     <div class="row">
       <div class="col s3 left">
-        <a href="#!"><i class="material-icons">brightness_1</i></a>
+        <div id="circle"></div>
+       <!--  <a href="#!"><i class="material-icons">brightness_1</i></a> -->
       </div>
       <div class="col s6 center-align">
          <div class="title">
@@ -10,12 +11,12 @@
          </div>
          <h2>Lorem ipsum dolor sit amet, eu est summo scripserit. Mei cu quod vol uptua</h2>
       </div>
-      <div class="col s3">
+      <!-- <div class="col s3"> -->
         <ul class="hide-on-med-and-down right">
           <li><b>MENU</b></li>
-          <li><a href=""><i class="material-icons">reorder</i></a></li>
+          <li><a href=""><i class="large material-icons">reorder</i></a></li>
         </ul>
-      </div>
+      <!-- </div> -->
     </div>
   </div>
 </nav>
