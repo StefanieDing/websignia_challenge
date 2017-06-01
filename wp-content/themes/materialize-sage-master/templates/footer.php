dddaa0423a885691aca40e747262338fe101f2b0
Stefanie Ding <StefanieDing@StefaniDingsMBP.home>
2017-05-31 22:51:18 -0400

    stylfooter styling finished

--- wp-content/themes/materialize-sage-master/templates/footer.php
+++ wp-content/themes/materialize-sage-master/templates/footer.php
@@ -1,21 +1,25 @@
 <footer class="page-footer">
   <div class="container center-align">
     <div class="row info">
-      <div class="col 4">
+      <div class="col s4">
         <h4>&copy; <?php echo date("Y") ?> LOREM IPSUM</h4>
       </div>
-       <div class="col 4">
+       <div class="col s4">
         <h4>80 LOREM ST, IPSUM, NJ 00000</h4>
       </div>
-       <div class="col 4">
+       <div class="col s4">
         <h4>555.555.5555</h4>
       </div>
     </div>
     <div class="row">
-      <h2>Lorem ipsum dolor sit amet, eu est summo scripserit</h2>
+      <div class="col s12">
+        <h2>Lorem ipsum dolor sit amet, eu est summo scripserit</h2>
+      </div>
     </div>
     <div class="row">
-      <img src="wp-content/themes/materialize-sage-master/assets/images/computer.png" alt="Computer">
+      <div class="col s12">
+        <img src="wp-content/themes/materialize-sage-master/assets/images/computer.png" alt="Computer">
+      </div>
     </div>
   </div>
 </footer>
