0c4a799597af29f542a24a8821d3e080bba7387c
Stefanie Ding <StefanieDing@StefaniDingsMBP.home>
2017-06-01 00:12:03 -0400

    content layout

--- wp-content/themes/materialize-sage-master/index.php
+++ wp-content/themes/materialize-sage-master/index.php
@@ -1,6 +1,6 @@
 <?php get_template_part('templates/page','header'); ?>
 
-<?php get_template_part('content'); ?>
+<?php get_template_part('templates/content'); ?>
 
 <?php get_template_part('footer'); ?>
 
--- wp-content/themes/materialize-sage-master/templates/content.php
+++ wp-content/themes/materialize-sage-master/templates/content.php
@@ -1,4 +1,4 @@
-<div class="col s12">
+<!-- <div class="col s12">
   <div <?php post_class('card'); ?>>
     <div class="card-content">
       <span class="card-title"><?php the_title(); ?></span>
@@ -9,4 +9,70 @@
       <a href="<?php the_permalink(); ?>">Read more</a>
     </div>
   </div>
-</div>
+</div> -->
+  <div class="row">
+    <div class="col s12">
+      <!-- img -->
+    </div>
+  </div>
+
+  <div class="row quote">
+    <div class="col s12">
+      <div class="smalltitle">SMALL TITLE</div>
+      <h2>Lorem ipsum dolor sit atem, eu est summo scripserit. Mei cu quod vol uptua</h2>
+    </div>
+  </div>
+
+  <!-- cateogories -->
+  <div class="row">
+    <div class="col s12">
+      <!-- first row -->
+      <div class="row">
+        <div class="col s4">
+        </div>
+        <div class="col s4">
+        </div>
+        <div class="col s4">
+        </div>
+      </div>
+      <!-- second row -->
+      <div class="row">
+        <div class="col s4">
+        </div>
+        <div class="col s4">
+        </div>
+        <div class="col s4">
+        </div>
+      </div>
+      <!-- third row -->
+      <div class="row">
+        <div class="col s4">
+        </div>
+        <div class="col s4">
+        </div>
+        <div class="col s4">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col s12">
+      <!-- img -->
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col s12">
+      <div class="smalltitle">SMALL TITLE</div>
+
+      <!-- list -->
+    </div>
+  </div>
+
+  <div class="row quote">
+    <div class="col s12">
+      <h2>Mei cu quod vol uptua elaboraret. An wisi hen drerit pertinacia vis, ne eum saepe vive ndum interesset. Lobortis sapi ent em no usu</h2>
+    </div>
+  </div>
+
